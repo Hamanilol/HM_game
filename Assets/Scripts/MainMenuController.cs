9e999b0a40345d573df934d70e74a76045d71a25
Sayed Ali Almusawi <202304831@student.polytechnic.bh>
2026-05-24 01:53:20 +0300

    new cursor

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -3,34 +3,72 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    [Header("Scene Names")]
     [SerializeField] private string singlePlayerScene = "SinglePlayerGameplay";
     [SerializeField] private string multiplayerScene = "MultiplayerGameplay";
     [SerializeField] private string optionsScene = "OptionsMenu";
-    [SerializeField] private string creditsScene = "CreditsMenu"; // New credits scene variable
+    [SerializeField] private string creditsScene = "CreditsMenu";
 
+    [Header("Audio Setup")]
+    [SerializeField] private AudioSource backgroundMusicSource;
+    [SerializeField] private AudioSource sfxSource;
+    [SerializeField] private AudioClip hoverSound;
+    [SerializeField] private AudioClip clickSound;
+
+    private void Start()
+    {
+        // Ensures background music starts playing immediately on menu load
+        if (backgroundMusicSource != null && !backgroundMusicSource.isPlaying)
+        {
+            backgroundMusicSource.Play();
+        }
+    }
+
+    // Public audio functions to be called by buttons
+    public void PlayHoverSound()
+    {
+        if (sfxSource != null && hoverSound != null)
+        {
+            sfxSource.PlayOneShot(hoverSound);
+        }
+    }
+
+    public void PlayClickSound()
+    {
+        if (sfxSource != null && clickSound != null)
+        {
+            sfxSource.PlayOneShot(clickSound);
+        }
+    }
+
+    // Scene Navigation functions
     public void PlaySinglePlayer()
     {
+        PlayClickSound();
         SceneManager.LoadScene(singlePlayerScene);
     }
 
     public void PlayMultiplayer()
     {
+        PlayClickSound();
         SceneManager.LoadScene(multiplayerScene);
     }
 
     public void OpenOptions()
     {
+        PlayClickSound();
         SceneManager.LoadScene(optionsScene);
     }
 
     public void OpenCredits()
     {
-        // Loads the credits scene when the button is clicked
+        PlayClickSound();
         SceneManager.LoadScene(creditsScene);
     }
 
     public void QuitGame()
     {
+        PlayClickSound();
         #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
         #else
