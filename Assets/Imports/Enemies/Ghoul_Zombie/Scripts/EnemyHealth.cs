be07dd9c5f3c1f10ec39dfd1fe603c1ba286caff
Hamanilol <abdulrahmanmusa325@gmail.com>
2026-05-31 23:45:23 +0300

    made boss hp UI reactive to hp

---
+++ Assets/Imports/Enemies/Ghoul_Zombie/Scripts/EnemyHealth.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using Abdulrahman.EnemySystem;
+
+namespace Abdulrahman.EnemySystem
+{
+    public class EnemyHealth : MonoBehaviour
+    {
+        public float maxHealth = 100f;
+        [Header("REWARD")]
+        public int moneyReward = 100;
+        
+        [Header("BOSS SETTINGS")]
+        public bool isBoss = false;
+        public string bossDisplayName = "Boss";
+
+        public float _currentHealth;
+        private BaseEnemyAI _enemyAI;
+        private CurrencyUI _currencyUI;
+
+        public event System.Action<float, float> OnHealthChanged;
+
+        private void Start()
+        {
+            _currentHealth = maxHealth;
+            _enemyAI = GetComponent<BaseEnemyAI>();
+            _currencyUI = FindFirstObjectByType<CurrencyUI>();
+            OnHealthChanged?.Invoke(_currentHealth, maxHealth);
+
+            if (isBoss)
+            {
+                var bossBar = Object.FindAnyObjectByType<Abdulrahman.UISystem.BossHealthBar>(FindObjectsInactive.Include);
+                if (bossBar != null)
+                {
+                    bossBar.InitializeBoss(this, bossDisplayName);
+                }
+            }
+        }
+
+        public void TakeDamage(float amount)
+        {
+            if (_currentHealth <= 0) return;
+
+            _currentHealth -= amount;
+            Debug.Log($"[EnemyHealth] {gameObject.name} took {amount} damage. HP: {_currentHealth}/{maxHealth}");
+            
+            OnHealthChanged?.Invoke(_currentHealth, maxHealth);
+
+            if (_currentHealth <= 0)
+            {
+                _currentHealth = 0;
+                
+                if (_currencyUI != null)
+                    _currencyUI.AddMoney(moneyReward);
+                
+                _enemyAI.Die();
+            }
+        }
+    }
+}
